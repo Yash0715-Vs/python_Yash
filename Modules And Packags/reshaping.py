810039d2cfb4ddbc9d4bdf6466c4d403665fdc9f
Yash0715-Vs <yashsuthar697@gmail.com>
2026-08-12 12:13:36 +0530

    initial commit

---
+++ Modules And Packags/reshaping.py
@@ -0,0 +1,9 @@
+import numpy as np
+# numbers = np.array([1, 2, 3, 4, 5, 6])
+# result = numbers.reshape(2, 3)
+# print(result)
+
+numbers = np.arange(1, 13)
+result = numbers.reshape(4, 3)
+print(numbers)
+print(result)
