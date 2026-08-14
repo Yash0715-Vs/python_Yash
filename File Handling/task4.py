c8af29d9345847ec97caa999619de0764f2c8e1c
Yash0715-Vs <yashsuthar697@gmail.com>
2026-08-14 11:31:46 +0530

    initial commit

---
+++ File Handling/task4.py
@@ -0,0 +1,9 @@
+name= input("enter the name: ")
+age= int(input("enter the age: "))
+marks= int(input("enter the marks: "))
+with open("File Handling/student.txt", "a") as file:
+    file.write("\nYash"
+               "\n21" 
+               "\n41")
+    
+
