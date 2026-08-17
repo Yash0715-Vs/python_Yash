0a4a2e72387778e5e683131ac436b838cbd8b15e
Yash0715-Vs <yashsuthar697@gmail.com>
2026-08-17 16:11:54 +0530

    initial commit

---
+++ JSON/eg1.py
@@ -0,0 +1,9 @@
+student = {
+    "name": "Yash",
+    "contact": {
+        "email":"yashsuthar@gmail.com",
+        "number": "193848593"
+    }
+}
+print(student)
+print(student["contact"]["email"])
