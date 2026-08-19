8083f8a180c04c0dbd22e3aaed3d72c137b2aa5d
Yash0715-Vs <yashsuthar697@gmail.com>
2026-08-19 11:57:35 +0530

    initial commit

---
+++ Unit testing/Test_calculator.py
@@ -0,0 +1,16 @@
+import unittest
+
+from calculator import add, substraction, multiply
+
+class TestCalculator(unittest.TestCase):
+
+    def test_add(self):
+        self.assertEqual(add(2,3),5)
+
+    def test_substraction(self):
+        self.assertNotEqual(substraction(4,4),1)
+
+    def test_multiply(self):
+        self.assertEqual(multiply(4,4),16)
+
+unittest.main()
