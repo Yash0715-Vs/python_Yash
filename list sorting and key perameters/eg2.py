be770c3c87d602fcc5add836bd4780f41d6ecb5c
Yash0715-Vs <yashsuthar697@gmail.com>
2026-08-19 16:49:11 +0530

    initial commit

--- list sorting and key perameters/eg2.py
+++ list sorting and key perameters/eg2.py
@@ -4,21 +4,3 @@ by_score = sorted(students, key=lambda s: s[1], reverse=True)
 
 print(by_score)
 
-words = ["banana", "fig", "apple"]
-
-print(sorted(words, key=len))
-
-employees = [
-    {"name": "Ravi", "salary": 45000},
-    {"name": "Anu", "salary": 60000},
-    {"name": "Kabir", "salary": 35000},
-    {"name": "Neha", "salary": 75000}
-]
-
-sorted_employees = sorted(
-    employees,
-    key=lambda employee: employee["salary"],
-    reverse=True
-)
-
-print(sorted_employees)
