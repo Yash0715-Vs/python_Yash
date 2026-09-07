0777bd23b5330f6544edb9ee32f3f526cb03a5e3
Yash0715-Vs <yashsuthar697@gmail.com>
2026-09-07 13:25:18 +0530

    initial commit

--- CORE PYTHON TASKS/dict task.py
+++ CORE PYTHON TASKS/dict task.py
@@ -3,46 +3,51 @@ student={
     'rollno':41,
     'name':"Yash",
     'marks':85,
-    'grade':'none',
+    'grade':None,
 },
 's2':{
     'rollno':42,
     'name':"Nimesh",
     'marks':90,
-    'grade':'none',
+    'grade':None,
 },
 's3':{
     'rollno':43,
     'name':"Aryan",
     'marks':75,
-    'grade':'none',
+    'grade':None,
 },
 's4':{
     'rollno':44,
     'name':"Sidharth",
     'marks':80,
-    'grade':'none',
+    'grade':None,
 },
 's5':{
     'rollno':45,
     'name':"Isha",
     'marks':85,
-    'grade':'none',
+    'grade':None,
 }
 
 }
 
 for key in student:
     if student[key]['marks']>=90 and student[key]['marks']<=100:
+        student[key]['grade'] = 'A'
         print(student[key]['grade'])
 
     elif student[key]['marks']>=80 and student[key]['marks']<90:
+        student[key]['grade'] = 'B'
         print(student[key]['grade'])
     elif student[key]['marks']>=70 and student[key]['marks']<80:
+        student[key]['grade'] = 'C'
         print(student[key]['grade'])
     elif student[key]['marks']>=60 and student[key]['marks']<70:
+        student[key]['grade'] = 'D'
         print(student[key]['grade'])
     elif student[key]['marks']>=40 and student[key]['marks']<60:
+        student[key]['grade'] = 'E'
         print(student[key]['grade'])
     else:
         print("Fail")
