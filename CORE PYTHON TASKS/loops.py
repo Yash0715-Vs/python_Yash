892b926a73b731d75f93d2183558e3ba51208404
Yash0715-Vs <yashsuthar697@gmail.com>
2026-09-08 10:58:49 +0530

    initial commit

---
+++ CORE PYTHON TASKS/loops.py
@@ -0,0 +1,10 @@
+x = input("Enter a character: ")
+if x == 'a':
+    print("you have entered a")
+elif x == 'b':
+    print("you have entered b")
+elif x == 'c':
+    print("you have entered c")
+
+else:
+    print("invalid character")
