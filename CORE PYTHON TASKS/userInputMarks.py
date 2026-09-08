102bb2cb98f0b553668c364c523066e8e9a5eabb
Yash0715-Vs <yashsuthar697@gmail.com>
2026-09-08 11:12:55 +0530

    initial commit

---
+++ CORE PYTHON TASKS/userInputMarks.py
@@ -0,0 +1,15 @@
+n= int(input("enter the marks: "))
+if n>=90 and n<=100:
+    print("A")
+elif n>=80 and n<90:
+    print("B")
+elif n>=60 and n<80:
+    print("C")
+elif n>=40 and n<60:
+    print("D")
+elif n<40:
+    print("fail")
+elif n>100:
+    print("Invalid marks")
+else:
+    print("error")
