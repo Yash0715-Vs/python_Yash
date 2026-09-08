41dda1d413bcd23fb249573512dbb3f34112da32
Yash0715-Vs <yashsuthar697@gmail.com>
2026-09-08 11:08:13 +0530

    initial commit

---
+++ CORE PYTHON TASKS/userInputEvenOdd.py
@@ -0,0 +1,24 @@
+n = int(input("enter the number: "))
+if n>0:
+
+    if n%2!=0:
+        result= n +10
+        print("positive odd number")
+        print(f"result is: {result}")
+    else:
+        result = n*2.5
+        print("positive even number")
+        print(f"result is: {result}")
+
+elif n<0:
+    if n%2!=0:
+        result= n - 10
+        print("negative odd number")
+        print(f"result is: {result}")
+    else:
+        result = n/2.5
+        print("negative even number")
+        print(f"result is: {result}")
+
+else:
+    print("number is zero")
