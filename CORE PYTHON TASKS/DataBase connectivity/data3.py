7bf21dadb9601014f47d699f1b75c7e5a938778f
Yash0715-Vs <yashsuthar697@gmail.com>
2026-09-22 17:48:44 +0530

    initial commit

--- CORE PYTHON TASKS/DataBase connectivity/data3.py
+++ CORE PYTHON TASKS/DataBase connectivity/data3.py
@@ -6,8 +6,8 @@ connection = pymysql.connect(
     password='root',
     db = "employee"
 )
-#cursor = connection.cursor()
-#query = """INSERT INTO employee (name,age, department, salary) VALUES (%s, %s, %s, %s)"""
-#cursor.execute(query, ("John Doe", 30, "IT", 50000))
-#connection.commit()
-#connection.close()
+cursor = connection.cursor()
+query = """INSERT INTO employee (name,age, department, salary) VALUES (%s, %s, %s, %s)"""
+cursor.execute(query, ("John Doe", 30, "IT", 50000))
+connection.commit()
+connection.close()
