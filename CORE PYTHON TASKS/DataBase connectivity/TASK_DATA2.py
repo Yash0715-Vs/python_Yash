990bda20f7cbce74124ff190f5f052a8fd085768
Yash0715-Vs <yashsuthar697@gmail.com>
2026-09-23 14:34:54 +0530

    initial commit

---
+++ CORE PYTHON TASKS/DataBase connectivity/TASK_DATA2.py
@@ -0,0 +1,36 @@
+import pymysql
+
+
+def insertLogin():
+    connection = pymysql.connect(
+        host='localhost',
+        port=3306,
+        user='root',
+        password='root',
+        db='userlogin'
+    )
+
+    cursor1 = connection.cursor()
+
+    cursor1.execute(
+        "insert into userlogin(id, first_name, last_name, username, pass, course) values (%s, %s, %s, %s, %s, %s)",
+        (41, 'YASH', 'Suthar', 'yash1234', 12345, 'IT')
+    )
+    cursor1.execute(
+        "insert into userlogin(id, first_name, last_name, username, pass, course) values (%s, %s, %s, %s, %s, %s)",
+        (157, 'baku', 'Suthar', 'baku1234', 22277, 'CSE')
+    )
+    cursor1.execute(
+        "insert into userlogin(id, first_name, last_name, username, pass, course) values (%s, %s, %s, %s, %s, %s)",
+        (557, 'DKFS', 'DSDF', 'KDJN1234', 22877, 'ITA')
+    )
+    cursor = connection.cursor()  
+    cursor.execute("SELECT * FROM userlogin")
+    cursor.execute("delete from userlogin where id= 41")
+    data = cursor.fetchall()
+    print(data)
+    print("Data fetched successfully")
+    connection.commit()
+
+
+insertLogin()
